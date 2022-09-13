77c0ebf6063c7482d53292c0873d492478d58619
Anil <billaanil3@gmail.com>
2022-09-13 11:12:14 +0530

    initial commit

---
+++ Project_1/app1/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Persons(models.Model):
+    PersonID  = models.IntegerField(primary_key=True)
+    LastName = models.CharField(max_length=20)
+    FirstName = models.CharField(max_length=20)
+    Address = models.CharField(max_length=200)
+    City = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.FirstName
+
+    # def __repr__(self):
+
---
+++ Project_1/app1/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.views.generic import View
+
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Persons
+
+# Create your views here.
+
+def welcome_message(request):
+    return HttpResponse("Welcome")
+
+
+def auth_user_details(request):
+    users = User.objects.all()
+    return HttpResponse(users)
+
+def even_numbers(request):
+    evens = [i for i in range(10) if i%2==0]
+    return HttpResponse(str(evens))
+
+class PersonDetails(View):
+    def get(self, request):
+        persons = Persons.objects.all()
+        return HttpResponse(persons)
+
+    @csrf_exempt
+    def post(self, request):
+        import pdb
+        pdb.set_trace()
+        data = request.POST['data']
+
